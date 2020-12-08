f356ed25453f52fc75e03fc05235c7b40acb9747
ckaiwen <469993222@qq.com>
2020-12-08 17:19:26 +0800

    测试通自签证书

--- client.go
+++ client.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"grpctest/services"
+	"io/ioutil"
 	"log"
 )
 
 func main() {
-	conn,err:= grpc.Dial(":8080",grpc.WithInsecure())
-	if err!= nil{
-		fmt.Println("dial err:",err)
+	cert,err:=tls.LoadX509KeyPair("client_cert/client.pem","client_cert/client.key")
+	if err!=nil{
+		log.Fatal("LoadX509KeyPair error:",err)
 	}
+	certPool:= x509.NewCertPool()
+	ca,err:=ioutil.ReadFile("client_cert/ca.pem")
+	if err!= nil{
+		log.Fatal("ioutil.ReadFile:",err)
+	}
+	certPool.AppendCertsFromPEM(ca)
 
+	creds:=credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ServerName: "localhost",
+		RootCAs: certPool,
+	})
+
+	conn,err:= grpc.Dial(":8080",grpc.WithTransportCredentials(creds))
 	prodClient:=services.NewProdServiceClient(conn)
 	prodRes,err:=prodClient.GetProdStock(context.Background(),&services.ProdRequest{ProdId: 15})
 	if err!=nil{
--- server.go
+++ server.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"grpctest/services"
+	"io/ioutil"
+	"log"
 	"net"
 )
 
 func main() {
-	rpcServer:=grpc.NewServer()
+	cert,err:=tls.LoadX509KeyPair("server_cert/server.pem","server_cert/server.key")
+	if err!=nil{
+		log.Fatal("LoadX509KeyPair error:",err)
+	}
+    certPool:= x509.NewCertPool()
+    ca,err:=ioutil.ReadFile("server_cert/ca.pem")
+    if err!= nil{
+    	log.Fatal("ioutil.ReadFile:",err)
+	}
+    certPool.AppendCertsFromPEM(ca)
+
+    creds:=credentials.NewTLS(&tls.Config{
+    	Certificates: []tls.Certificate{cert},
+    	ClientAuth: tls.RequireAndVerifyClientCert,
+    	ClientCAs: certPool,
+	})
+	rpcServer:=grpc.NewServer(grpc.Creds(creds))
+
 	services.RegisterProdServiceServer(rpcServer,new(services.ProdService))
 	listen,_:= net.Listen("tcp",":8080")
 	rpcServer.Serve(listen)
